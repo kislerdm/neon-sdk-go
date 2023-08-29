a4b348d9da428a07920238859cf036300d66331c
kislerdm <kislerdm@users.noreply.github.com>
2023-08-29 00:08:16 +0000

    feat: auto-updated SDK
    Signed-off-by: GitHub <noreply@github.com>

--- sdk.go
+++ sdk.go
@@ -1510,7 +1510,7 @@ type RolesResponse struct {
 	Roles []Role `json:"roles"`
 }
 
-// SuspendTimeoutSeconds Duration of inactivity in seconds after which the cimpute endpoint is
+// SuspendTimeoutSeconds Duration of inactivity in seconds after which the compute endpoint is
 // automatically suspended. The value `0` means use the global default.
 // The value `-1` means never suspend. The default value is `300` seconds (5 minutes).
 // The maximum value is `604800` seconds (1 week). For more information, see
