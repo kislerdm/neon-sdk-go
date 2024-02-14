fb0e29909ddc06384447c64d5eb41d0302c555b2
kislerdm <13434797+kislerdm@users.noreply.github.com>
2024-02-14 00:08:43 +0000

    feat: auto-updated SDK
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- sdk.go
+++ sdk.go
@@ -1524,6 +1524,7 @@ type RolesResponse struct {
 // SuspendTimeoutSeconds Duration of inactivity in seconds after which the compute endpoint is
 // automatically suspended. The value `0` means use the global default.
 // The value `-1` means never suspend. The default value is `300` seconds (5 minutes).
+// The minimum value is `60` seconds (1 minute).
 // The maximum value is `604800` seconds (1 week). For more information, see
 // [Auto-suspend configuration](https://neon.tech/docs/manage/endpoints#auto-suspend-configuration).
 type SuspendTimeoutSeconds int64
