440c04c671ab47360cee7351c27a38e5855b4c67
kislerdm <13434797+kislerdm@users.noreply.github.com>
2024-10-11 00:11:20 +0000

    feat: auto-updated SDK
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- sdk.go
+++ sdk.go
@@ -1975,12 +1975,13 @@ type ProjectsResponse struct {
 
 // Provisioner The Neon compute provisioner.
 // Specify the `k8s-neonvm` provisioner to create a compute endpoint that supports Autoscaling.
+//
+// Provisioner can be one of the following values:
+// * k8s-pod
+// * k8s-neonvm
+//
+// Clients must expect, that any string value that is not documented in the description above should be treated as a error. UNKNOWN value if safe to treat as an error too.
 type Provisioner string
-
-const (
-	ProvisionerK8sNeonvm Provisioner = "k8s-neonvm"
-	ProvisionerK8sPod    Provisioner = "k8s-pod"
-)
 
 type Role struct {
 	// BranchID The ID of the branch to which the role belongs
