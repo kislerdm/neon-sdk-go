0b61133009f111313b908695d95e1246df2f1516
Dmitry Kisler <admin@dkisler.com>
2024-12-08 12:18:59 +0100

    update license dates
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- e2e-example/main.go
+++ e2e-example/main.go
@@ -22,13 +22,16 @@ func main() {
 		log.Printf("could not initialize SDK: %v\n", err)
 	}
 
+	// provision a project with default configurations
 	resp, err := client.CreateProject(sdk.ProjectCreateRequest{})
 	if err != nil {
 		log.Printf("could not create the project: %v\n", err)
 	}
+	// delete the provisioned project in the end of the process' execution
+	defer func() { _, _ = client.DeleteProject(resp.ProjectResponse.Project.ID) }()
 
+	// connect to the default database
 	connectionURI := resp.ConnectionURIs[0].ConnectionURI
-
 	ctx := context.Background()
 	conn, err := pgx.Connect(ctx, connectionURI)
 	if err != nil {
@@ -36,8 +39,11 @@ func main() {
 	}
 	defer func() { _ = conn.Close(ctx) }()
 
+	// query the database
 	r, _ := conn.Query(ctx, "select now() at time zone 'utc';")
 	defer func() { r.Close() }()
+
+	// read query result to stdout
 	for r.Next() {
 		var now time.Time
 		if e := r.Scan(&now); e != nil {
