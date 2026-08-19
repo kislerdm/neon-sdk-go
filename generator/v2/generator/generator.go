f848b664ebac5f118d419468bfd25777bb54fc1f
Dmitry Kisler <admin@dkisler.com>
2026-08-19 17:43:37 +0200

    add data types for an adapter to openAPI data structure
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- generator/v2/generator/generator.go
+++ generator/v2/generator/generator.go
@@ -4,14 +4,18 @@ import (
 	"encoding/json"
 	"errors"
 	"math"
+	"sort"
 )
 
 type OpenAPIResponse struct {
+	// xRefName defines the name of the response if it was defined in the `components.responses` section.
+	xRefName    string
 	Description string
 	Schema      OpenAPISchema
+	Ref         *string
 }
 
-func (o *OpenAPIResponse) UnmarshalJSON(v []byte) error {
+func (v *OpenAPIResponse) UnmarshalJSON(data []byte) error {
 	var tmp struct {
 		Description string `json:"description"`
 		Content     struct {
@@ -19,16 +23,21 @@ func (o *OpenAPIResponse) UnmarshalJSON(v []byte) error {
 				Schema OpenAPISchema `json:"schema"`
 			} `json:"application/json"`
 		} `json:"content"`
+		Ref *string `json:"$ref,omitempty"`
 	}
-	if err := json.Unmarshal(v, &tmp); err != nil {
+	if err := json.Unmarshal(data, &tmp); err != nil {
 		return err
 	}
-	o.Description = tmp.Description
-	o.Schema = tmp.Content.ApplicationJson.Schema
+	v.Description = tmp.Description
+	v.Schema = tmp.Content.ApplicationJson.Schema
+	v.Ref = tmp.Ref
 	return nil
 }
 
 type OpenAPISchema struct {
+	// xRefName defines the name of the schema if it was defined in the `components.schemas` section.
+	xRefName string
+
 	Type        string                   `json:"type,omitempty"`
 	Format      *string                  `json:"format,omitempty"`
 	Enum        []string                 `json:"enum,omitempty"`
@@ -43,6 +52,8 @@ type OpenAPISchema struct {
 }
 
 type OpenAPIParameter struct {
+	// xRefName defines the name of the parameter if it was defined in the `components.parameters` section.
+	xRefName    string
 	Name        string        `json:"name"`
 	In          string        `json:"in"`
 	Description string        `json:"description"`
@@ -50,13 +61,120 @@ type OpenAPIParameter struct {
 }
 
 type Components struct {
-	Responses  map[string]OpenAPIResponse  `json:"responses"`
-	Schemas    map[string]OpenAPISchema    `json:"schemas"`
-	Parameters map[string]OpenAPIParameter `json:"parameters"`
+	Responses  []OpenAPIResponse
+	Schemas    []OpenAPISchema
+	Parameters []OpenAPIParameter
+}
+
+func (v *Components) UnmarshalJSON(data []byte) error {
+	var tmp struct {
+		Responses  map[string]OpenAPIResponse  `json:"responses"`
+		Schemas    map[string]OpenAPISchema    `json:"schemas"`
+		Parameters map[string]OpenAPIParameter `json:"parameters"`
+	}
+	if err := json.Unmarshal(data, &tmp); err != nil {
+		return err
+	}
+
+	v.Responses = make([]OpenAPIResponse, 0, len(tmp.Responses))
+	for _, k := range sortMapKeys(tmp.Responses) {
+		vv := tmp.Responses[k]
+		vv.xRefName = k
+		v.Responses = append(v.Responses, vv)
+	}
+
+	v.Schemas = make([]OpenAPISchema, 0, len(tmp.Schemas))
+	for _, k := range sortMapKeys(tmp.Schemas) {
+		vv := tmp.Schemas[k]
+		vv.xRefName = k
+		v.Schemas = append(v.Schemas, vv)
+	}
+
+	v.Parameters = make([]OpenAPIParameter, 0, len(tmp.Parameters))
+	for _, k := range sortMapKeys(tmp.Parameters) {
+		vv := tmp.Parameters[k]
+		vv.xRefName = k
+		v.Parameters = append(v.Parameters, vv)
+	}
+
+	return nil
+}
+
+type OpenAPIPathMethodResponses struct {
+	Code200 *OpenAPIResponse `json:"200,omitempty"`
+	Code201 *OpenAPIResponse `json:"201,omitempty"`
+	Default OpenAPIResponse  `json:"default"`
+}
+
+type OpenAPIPathMethodRequestBody struct {
+	Required bool
+	Schema   OpenAPISchema
+}
+
+func (v *OpenAPIPathMethodRequestBody) UnmarshalJSON(data []byte) error {
+	var tmp struct {
+		Required bool `json:"required"`
+		Content  struct {
+			ApplicationJson struct {
+				Schema OpenAPISchema `json:"schema"`
+			} `json:"application/json"`
+		} `json:"content"`
+	}
+	if err := json.Unmarshal(data, &tmp); err != nil {
+		return err
+	}
+	v.Required = tmp.Required
+	v.Schema = tmp.Content.ApplicationJson.Schema
+	return nil
+}
+
+type OpenAPIPathMethod struct {
+	Description string                       `json:"description"`
+	OperationID string                       `json:"operationId"`
+	Parameters  []OpenAPIParameter           `json:"parameters,omitempty"`
+	Responses   OpenAPIPathMethodResponses   `json:"responses"`
+	RequestBody OpenAPIPathMethodRequestBody `json:"requestBody"`
+}
+
+type OpenAPIPath struct {
+	URLPath string
+
+	Parameters []OpenAPIParameter `json:"parameters,omitempty"`
+	Get        *OpenAPIPathMethod `json:"get,omitempty"`
+	Post       *OpenAPIPathMethod `json:"post,omitempty"`
+	Delete     *OpenAPIPathMethod `json:"delete,omitempty"`
+	Put        *OpenAPIPathMethod `json:"put,omitempty"`
+	Patch      *OpenAPIPathMethod `json:"path,omitempty"`
 }
 
 type OpenAPIDefinition struct {
-	Components Components `json:"components"`
+	Paths      []OpenAPIPath
+	Components Components
+}
+
+func (v *OpenAPIDefinition) UnmarshalJSON(data []byte) error {
+	var tmp struct {
+		Paths      map[string]OpenAPIPath `json:"paths"`
+		Components Components             `json:"components"`
+	}
+	if err := json.Unmarshal(data, &tmp); err != nil {
+		return err
+	}
+	v.Paths = make([]OpenAPIPath, 0, len(tmp.Paths))
+	for _, k := range sortMapKeys(tmp.Paths) {
+		v.Paths = append(v.Paths, tmp.Paths[k])
+	}
+	v.Components = tmp.Components
+	return nil
+}
+
+func sortMapKeys[E ~map[string]V, V OpenAPISchema | OpenAPIParameter | OpenAPIResponse | OpenAPIPath](v E) []string {
+	var o = make([]string, 0, len(v))
+	for k := range v {
+		o = append(o, k)
+	}
+	sort.Strings(o)
+	return o
 }
 
 func newGoType(schema OpenAPISchema) (t string, isStruct bool, err error) {
